3c92bcc32f3fedaf9b9b0f834b45e68e7bcb6fb3
tanvir35web <islammdtanvirul@gmail.com>
2023-09-07 15:10:32 +0600

    battery condition change

---
+++ script.js
@@ -0,0 +1,13 @@
+var x, y, sum, minus, multiply;
+x = 5;
+y = 8;
+
+sum = x + y;
+minus= x - y;
+multiply = x * y;
+
+document.getElementById('javascript').innerHTML = "<b>" + sum+"</b> <br> "+minus;
+ 
+ console.log(multiply);
+ 
+ 
