92558ee487b393419be398a2132c7f9c72c28833
tanvir35web <islammdtanvirul@gmail.com>
2023-09-30 11:47:27 +0600

    .

---
+++ LOOP/New folder/crush curse.js
@@ -0,0 +1,27 @@
+// function operation(a,b){
+//     var c = a + b ;
+//     var d = a - b ;
+
+//     console.log(c,d);
+// }
+// operation(10,555);
+
+
+var peoples = [
+    {
+        name : 'Reza',
+        age : 23
+    },
+    {
+        name : "Tarek",
+        age : 33
+    }
+]
+ 
+
+// peoples.forEach(function(people){
+//    let e = people.name;
+//    console.log(e);
+// })
+
+console.log(peoples.name)
---
+++ LOOP/New folder/pra.js
@@ -0,0 +1,7 @@
+function operation(a,b){
+    var c = a + b ;
+  var d = a - b ;
+
+   console.log(c,d);
+}
+operation(10,555);
